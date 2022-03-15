464d91ef7ff51f0d9818eb037fd6af276a290d58
Aaron5208046 <41503676+Aaron5208046@users.noreply.github.com>
2022-03-15 18:20:00 +1100

    Fixed URL and added disease scraping

--- scraper.py
+++ scraper.py
@@ -5,9 +5,11 @@ from pprint import pprint
 web_data = []
 
 # Scraping first 10 pages
-count = 0
-while count != 10:
-    url = 'https://www.cidrap.umn.edu/news-perspective?page='+str(count)
+# First page starts at 0
+# For testing, using only 1 page for faster results
+page_num = 0
+while page_num != 1:
+    url = 'https://www.cidrap.umn.edu/news-perspective?page='+str(page_num)
 
     data = requests.get(url)
     html = BeautifulSoup(data.text, 'html.parser')
@@ -18,11 +20,42 @@ while count != 10:
     for article in articles:
         sub_art = article.select('div.views-field.views-field-rendered-entity')
         for sub in sub_art:
-            date = sub.select_one('.date-display-single').text
-            url = "cidrap.umn.edu"+ sub.select_one('.node-title.fieldlayout.node-field-title a')['href']     
-            headline = sub.select_one('.node-title.fieldlayout.node-field-title a').text                      
-            web_data.append({"url": url, "date": date, "headline": headline})
             
-    count += 1        
+            reports = []
+            
+            date = sub.select_one('.date-display-single').text
+            art_url = "http://cidrap.umn.edu"+ sub.select_one('.node-title.fieldlayout.node-field-title a')['href']  
+            
+            # Opening article to be scraped
+            art_data = requests.get(art_url)
+            art_html = BeautifulSoup(art_data.text, 'html.parser')
+            
+            report = art_html.select_one('div.node-inner')
+            
+            # TODO
+            #location
+            #event date
+            #syndrome
+            
+            # Scraping the diseases mentioned in the article
+            filed_under = report.select_one('div.fieldlayout-inline.fieldlayout.node-field-filed_under')
+            diseases_filed = filed_under.select('div.field-items')
+            diseases = []
+            for d in diseases_filed:
+                disease = d.select_one('.field-item.even a').text
+                diseases.append(disease)    
+            reports.append({"disease": diseases})
+            
+            
+            #TO FIX - Scans with dot points as main texts 
+            try:
+                main_text = sub.select_one('.field-item.even p').text   
+            except AttributeError:
+                main_text = "None"  
+            
+            headline = sub.select_one('.node-title.fieldlayout.node-field-title a').text                      
+            web_data.append({"url": art_url, "date_of_publication": date, "headline": headline, "main_text": main_text, "report": reports})
+            
+    page_num += 1        
 
 pprint(web_data)
