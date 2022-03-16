0de5cc96e5f65d632bfdbe5b87d13ff33016afb3
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2022-03-16 11:32:51 +1100

    updated main.py

--- PHASE_1/API_SourceCode/main.py
+++ PHASE_1/API_SourceCode/main.py
@@ -3,11 +3,27 @@ Base API Interface linking the web service to other
 resources.
 """
 
-from typing import Optional
+from typing import List, Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
 # Base Input and Response Models
+class SignUpInfo(BaseModel):
+    """
+    To be used later to collect account information
+    """
+    email: str
+    username: str
+    name: str
+    password: str
+
+class LoginInfo(BaseModel):
+    """
+    To be used later for Login and Logout.
+    """
+    username: str
+    password: str
+
 class SearchItem(BaseModel):
     """
     To be later modified depending on DB Implementation
@@ -17,15 +33,27 @@ class SearchItem(BaseModel):
     start_date: str
     end_date: str
     timezone: Optional[str] = None
+    # To be used for account logging
+    account_id: Optional[str] = None
 
 class Report(BaseModel):
     """
     As per spec
     """
-    diseases: list
-    syndrome: list
-    event_date: list
-    locations: list
+    diseases: List[str] = None
+    syndrome: List[str] = None
+    event_date: List[str]
+    locations: List[str]
+
+    class Config:
+        schema_extra = {
+             "example": {
+                    "diseases": ["RandoVirus"],
+                    "syndrome": None,
+                    "event_date": ["2015-10-01T08:45:10"],
+                    "locations": ["1234567"]
+                }
+        }
 
 class ArticleJson(BaseModel):
     """
@@ -35,19 +63,64 @@ class ArticleJson(BaseModel):
     date_of_publication: str
     headline: str
     main_text: str
-    reports: list
+    reports: List[Report]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "url": "www.example.com",
+                "date_of_publication": "2015-10-01T08:45:10",
+                "headline": "Pandemic Caused by RandoVirus",
+                "main_text": "Randovirus is a random virus...",
+                "reports": {
+                    "diseases": ["RandoVirus"],
+                    "syndrome": None,
+                    "event_date": ["2015-10-01T08:45:10"],
+                    "locations": ["1234567"]
+                }
+            }
+        }
+
+class HealthCheck(BaseModel):
+    """
+    As per spec
+    """
+    message: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "message": "Hello World!"
+            }
+        }
+
+class SearchHistory(BaseModel):
+    top_five_key_terms: List[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "top_five_key_terms": ["Zika", "Rhinovirus", "Coronavirus", "Smallpox", "Measles"]
+            }
+        }
 
 
 # API
 app = FastAPI()
 
-app.get('/hello')
-async def hello():
+@app.get(
+    '/healthcheck',
+    response_model = HealthCheck
+)
+async def healtchcheck():
     return {
-        'message': "Hello World"
+        'message': "Hello World!"
     }
 
-app.get('/search')
+@app.get(
+    '/search',
+    response_model = ArticleJson
+)
 async def search(
     search_item: SearchItem = Body(
         ...,
@@ -58,15 +131,28 @@ async def search(
             },
             "start_date": "2015-10-01T08:45:10",
             "end_date": "2015-11-01T19:37:12",
-            "timezone": None
+            "timezone": None,
+            "account_id": None
         }
     ),
 ):
     # Error checking dates and location
     # Add to search metrics
-    # Hard hardwired value if passes all correct values
+    # Pass hardcoded value if passes all correct values
     pass
 
-app.get('/search/key-frequency')
+@app.get(
+    '/search/key-frequency',
+    response_model = SearchHistory
+)
 async def key_frequency():
+    # Obtain most frequently searched keys in DB
+    pass
+
+@app.get(
+    '/search/history',
+    response_model =  SearchHistory
+)
+async def search_history():
+    # Right now it is to get the global search history
     pass
