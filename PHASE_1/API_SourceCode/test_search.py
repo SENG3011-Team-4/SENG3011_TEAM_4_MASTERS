b3175d44d61d8cf5e1f3ee019aadef34c03c7ec0
okfineolson <47872398+okfineolson@users.noreply.github.com>
2022-03-17 21:48:39 -0700

    small test for search function

---
+++ PHASE_1/API_SourceCode/test_search.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("..")
+from API_SourceCode.Search import *
+import pytest
+import re
+
+def test_search_v1():
+	
+	with pytest.raises(ValueError):
+		search_v1("anything","United state","2002-01-01T01:01:01","2000-01-01T01:01:01",Timezone = "UTC")
+		
+	with pytest.raises(ValueError):
+		search_v1("anything","United state","1","2",Timezone = "UTC")
