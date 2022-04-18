c13489cdf41e3a679b130f7d11b728314a031d5b
Adrian Borjigin <z5205791@ad.unsw.edu.au>
2022-04-18 12:19:32 +1000

    google maps api added circles for mouse hover info of diseases

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-from PHASE_1.API_SourceCode.medicine import common_disease_search, location_medication
+#from PHASE_1.API_SourceCode.medicine import common_disease_search, location_medication
 import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    example = {
+        "location": "Australia",
+        "disease": ["COVID-19", "Influenza"]
+    }
+    return render_template('home.html', data=example)
 
 @app.route("/login")
 def login():
@@ -19,7 +23,6 @@ def healthCheck():
 
 @app.route("/search")
 def search():
-    #exec(open("./PHASE_1/medicine.py").read())
     return render_template('search.html')
 
 @app.route("/settings")
